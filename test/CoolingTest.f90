421642e914929e854474ab052bd89b5c35a68fc2
Nolan <nolanmcmahonemail@gmail.com>
2023-12-19 11:13:41 -0700

    [RED] Added Test for Cooling Type Contract

--- test/CoolingTest.f90
+++ test/CoolingTest.f90
@@ -2,7 +2,7 @@ module CoolingTest
   use iso_fortran_env, only: int32, real32
   use testdrive, only: new_unittest, unittest_type, error_type, &
     check, test_failed, skip_test
-  !use Farseal, only: cooling
+  use Farseal, only: CoolingType
   implicit none
 
   private
@@ -21,6 +21,16 @@ module CoolingTest
 
     subroutine test_cooling(error)
       type(error_type), allocatable, intent(out) :: error
+      type(CoolingType), allocatable :: Cooler
+      real(kind=real32) :: starting_temp, ending_temp
+
+      starting_temp = Cooler%temp
+      call Cooler%cool()
+      ending_temp = Cooler%temp
+
+      call check(ending_temp < starting_temp)
+      if (allocated(error)) return
+
     end subroutine test_cooling
 
 end module CoolingTest
