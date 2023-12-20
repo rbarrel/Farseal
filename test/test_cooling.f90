3faff302ca27d786f72b4230abb3da4404a5c3ed
Nolan <nolanmcmahonemail@gmail.com>
2023-12-20 16:36:47 -0700

    [RED] Added Test for Custom Cooling Functions

--- test/test_cooling.f90
+++ test/test_cooling.f90
@@ -14,7 +14,8 @@ module Cooling
       type(unittest_type), allocatable, dimension(:), intent(out) :: testsuite
 
       testsuite = [ &
-        new_unittest("Test Cooling Interface", test_cooling) &
+        new_unittest("Test Cooling Interface", test_cooling), &
+        new_unittest("Test Custom Cooling", test_custom_cooling) &
       ]
 
     end subroutine collect_cooling_suite
@@ -35,5 +36,32 @@ module Cooling
       if (allocated(error)) return
 
     end subroutine test_cooling
+
+    subroutine test_custom_cooling(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(CoolingType), allocatable :: Cooler
+      real(kind=real32) :: starting_temp, ending_temp
+
+      Cooler = CoolingType()
+      call Cooler%init()
+      Cooler%method = CoolingMethods%Custom
+      Cooler%cool => FooBar
+      starting_temp = Cooler%temp
+      call Cooler%cool()
+      ending_temp = Cool%temp
+
+      call check(error, ending_temp < starting_temp)
+      if (allocated(error)) return
+
+      contains
+
+        subroutine FooBar(self)
+          class(CoolingType) :: self
+
+          self%temp = self%temp - 1
+
+        end subroutine FooBar
+
+    end subroutine test_custom_cooling
 
 end module Cooling
